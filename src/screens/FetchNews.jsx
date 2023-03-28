760cbf46e675f42fdc56160d54b5937493d5dbce
Your Name <you@example.com>
2023-03-28 14:17:46 -0300

    bosta

--- src/screens/FetchNews.jsx
+++ src/screens/FetchNews.jsx
@@ -1,22 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
-import { Styles } from "../utils/styles";
+import { ScrollView } from "react-native-web";
+import styles from "../utils/styles";
+//import {ScrollView} from "react-native";
 
+// fetch puxa uma função do javascript de um lugar
+//".then" o "reponse" e e transforma em "json"
 export default function FetchNews() {
+  const [data, setData] = useState([]);
+  const [nome, setNome] = useState("Isa");
   function FetchNewsData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then((json) => setData(json))
       .then((error) => console.error(error));
   }
   useEffect(() => {
     FetchNewsData();
   }, []);
 
+  // perguntar para o sor o serve o que está abaixo
+  // variavel da expondo a variavel "data"
   return (
-    <View>
+    <ScrollView style={styles.container}>
+      <View style={styles.container}>
       <Text>Fetch News</Text>
-    </View>
+      {data.map((item) => (
+        <Text key={item.id}>{item.title}</Text>
+      ))}
+      </View>
+    </ScrollView>
   );
 }
